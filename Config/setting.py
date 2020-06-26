eff6c2566bbab70ef66ad4c593a0ee8cde5d4c37
Origami <q@timvel.com>
2020-06-26 21:55:05 +0800

    chore

--- Config/setting.py
+++ Config/setting.py
@@ -53,17 +53,20 @@ DATABASES = {
 
 PROXY_GETTER = [
     "freeProxy01",
-    # "freeProxy02",
+    "freeProxy02",
     "freeProxy03",
     "freeProxy04",
     "freeProxy05",
-    # "freeProxy06",
+    "freeProxy06",
     "freeProxy07",
-    # "freeProxy08",
+    "freeProxy08",
     "freeProxy09",
+    "freeProxy10",
+    "freeProxy11",
+    "freeProxy12"
     "freeProxy13",
     "freeProxy14",
-    "freeProxy14",
+    "freeProxy15",
 ]
 
 """ API config http://127.0.0.1:5010 """
